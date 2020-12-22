fc386f99dbfb3fcc4bd0fc883b7a7cd50b5d5b3f
ekzemplaro <linux@hi-ho.ne.jp>
2020-12-22 17:32:02 +0900

    Commit on Dec/22/2020

--- ebay/data/maria_create.sql
+++
@@ -1,4 +0,0 @@
-use ebay;
-drop table if exists parts;
-create table parts (id int auto_increment primary key,operator varchar (8),asin varchar (20),dup tinyint (1),used tinyint (1),date_mod date);
-quit
--- ebay/display/py/html_src/go_html_gen.sh
+++
@@ -1,5 +0,0 @@
-HTML_CREATE=/var/www/data_base/lang/examples/bash/html_create
-#
-$HTML_CREATE/html_create.sh py /tmp/tmp0098.html
-#
-awk '!/Return/{print $0}' /tmp/tmp0098.html >  display_src.html
