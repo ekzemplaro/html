29e2235a6d0d049591ecd2f9335de84f9efca5f5
ekzemplaro <linux@hi-ho.ne.jp>
2023-05-05 08:32:34 +0900

    Commit on May/05/2023

--- index_visitors.js
+++ index_visitors.js
@@ -1,18 +1,31 @@
 // -----------------------------------------------------------------------
 //	index_visitors.js
 //
-//					Apr/25/2021
+//					May/05/2023
 //
 // -----------------------------------------------------------------------
-jQuery (function ()
+window.onload = ()=>
 {
+	document.querySelector("#outarea_aa").innerHTML
+		= "*** index_visitors.js *** start ***"
+
 	const url_php = "./index_visitors.php"
 
-	jQuery.get (url_php,function (data_received)
-                {
-		jQuery("#visitors").html (data_received)
-		})
+	fetch(url_php).then((response) => {
+		if(!response.ok) {
+			console.log('Read error!')
+			throw new Error('error')
+		} 
+		console.log('Read ok!')
+		return response.text()
+	}).then((data)  => {
+//		console.log(data)
+document.querySelector("#visitors").innerHTML = data
 
 })
+
+	document.querySelector("#outarea_hh").innerHTML
+		= "*** index_visitors.js *** end ***"
+}
 
 // -----------------------------------------------------------------------
