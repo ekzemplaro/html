99d9db1e2486ed20ed2277debc0c4135bb04fc3f
ekzemplaro <linux@hi-ho.ne.jp>
2025-02-12 12:28:06 +0900

    Commit on Feb/12/2025

--- test_dir/mqtt/button/nodejs/publish.js
+++ test_dir/mqtt/button/nodejs/publish.js
@@ -1,25 +1,26 @@
-const mqtt = require('mqtt');
+const mqtt = require('mqtt')
 
 // WebSocket エンドポイント
-const brokerUrl = 'ws://broker.hivemq.com:8000/mqtt';
+const brokerUrl = 'wss://broker.hivemq.com:8884/mqtt'
 
 // MQTT クライアントの作成
-const client = mqtt.connect(brokerUrl);
+const client = mqtt.connect(brokerUrl)
 
 client.on('connect', () => {
-  console.log('Connected to MQTT broker');
+  console.log('Connected to MQTT broker')
 
   // メッセージをパブリッシュ
-  client.publish('testaa/topic', 'Good Afternoon from MQTT.js', (err) => {
+  const message = 'Good Afternoon Feb/12/2025'
+  client.publish('testaa/topic', message, (err) => {
     if (!err) {
-      console.log('Message published');
+      console.log('Message published')
     } else {
-      console.error('Publish error:', err);
+      console.error('Publish error:', err)
     }
-    client.end(); // 接続を閉じる
-  });
-});
+    client.end() // 接続を閉じる
+  })
+})
 
 client.on('error', (err) => {
-  console.error('Connection error:', err);
-});
+  console.error('Connection error:', err)
+})
--- test_dir/mqtt/button/nodejs/subscribe.js
+++ test_dir/mqtt/button/nodejs/subscribe.js
@@ -1,26 +1,26 @@
 const mqtt = require('mqtt');
 
 // WebSocket エンドポイント
-const brokerUrl = 'ws://broker.hivemq.com:8000/mqtt';
+const brokerUrl = 'wss://broker.hivemq.com:8884/mqtt'
 
 // MQTT クライアントの作成
-const client = mqtt.connect(brokerUrl);
+const client = mqtt.connect(brokerUrl)
 
 client.on('connect', () => {
-  console.log('Connected to MQTT broker');
+  console.log('Connected to MQTT broker')
 
   // トピックをサブスクライブ
   client.subscribe('testaa/topic', (err) => {
     if (!err) {
-      console.log('Subscribed to testaa/topic');
+      console.log('Subscribed to testaa/topic')
     }
-  });
-});
+  })
+})
 
 client.on('message', (topic, message) => {
-  console.log(`Received message on ${topic}: ${message.toString()}`);
-});
+  console.log(`Received message on ${topic}: ${message.toString()}`)
+})
 
 client.on('error', (err) => {
-  console.error('Connection error:', err);
-});
+  console.error('Connection error:', err)
+})
