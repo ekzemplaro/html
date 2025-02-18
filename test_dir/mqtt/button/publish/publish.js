c19e8c2c7f63dbac8710b555d61bfa9b3ec2b6e4
ekzemplaro <linux@hi-ho.ne.jp>
2025-02-18 12:28:22 +0900

    Commit on Feb/18/2025

--- test_dir/mqtt/button/publish/publish.js
+++ test_dir/mqtt/button/publish/publish.js
@@ -41,26 +41,59 @@ client.on('error', (err) => {
 // const topic = '/topic/qos0'
 const topic = '/iwasaki/links2'
 
+const id_aa_on = document.getElementById('aa_on')
+const id_aa_off = document.getElementById('aa_off')
+const id_bb_on = document.getElementById('bb_on')
+const id_bb_off = document.getElementById('bb_off')
+
 document.getElementById('aa_on').addEventListener('click', () => {
-	const message = 'AA On PM 16:21'
+	
+	const message =  message_gen_proc('aa','on')
 	publish_proc(topic,message)
+	id_aa_on.style.color = "blue"
+	id_aa_off.style.color = "black"
 	})
 
 document.getElementById('aa_off').addEventListener('click', () => {
-	const message = 'AA Off PM 16:21'
+	const message =  message_gen_proc('aa','off')
 	publish_proc(topic,message)
+	id_aa_off.style.color = "blue"
+	id_aa_on.style.color = "black"
 	})
 document.getElementById('bb_on').addEventListener('click', () => {
-	const message = 'BB On PM 16:21'
+	const message =  message_gen_proc('bb','on')
 	publish_proc(topic,message)
+	bb_on.style.color = "blue"
+	bb_off.style.color = "black"
 	})
 
 document.getElementById('bb_off').addEventListener('click', () => {
-	const message = 'BB Off PM 16:21'
+	const message =  message_gen_proc('bb','off')
 	publish_proc(topic,message)
+	bb_on.style.color = "black"
+	bb_off.style.color = "blue"
 	})
 
+// -------------------------------------------------------------
+function message_gen_proc(device,status)
+{
+	let data_aa = new Object ()
+	data_aa['device'] = device
+	data_aa['status'] = status
+	data_aa['date'] = get_current_date_proc()
+	const message = JSON.stringify(data_aa)
 
+	return message
+}
 
+// -------------------------------------------------------------
+function get_current_date_proc()
+{
+	const today = new Date ()
+	var ddx = today.getFullYear () + "-" + (today.getMonth () +1)
+	ddx += "-" + today.getDate ()
+
+	return ddx
+}
 
 // -------------------------------------------------------------
