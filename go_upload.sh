d6a5ad0272a6f194e0999d384e7e422f21d8745f
ekzemplaro <linux@hi-ho.ne.jp>
2025-04-20 10:32:03 +0900

    Commit on Apr/20/2025

--- go_upload.sh
+++ go_upload.sh
@@ -1,10 +1,13 @@
 #
+# sudo ufw disable
+# sudo ufw status
 #
 git add --all
-git commit -m "Commit on Apr/19/2025"
+git commit -m "Commit on Apr/20/2025"
 #
 # git push -u origin master
 git push origin HEAD:master
 # git push
 #
+# sudo ufw enable
 #
