bedd88fb084beb0282b987e94e7c92c0fd859561
ekzemplaro <linux@hi-ho.ne.jp>
2025-06-01 12:51:32 +0900

    Commit on Jun/02/2025

---
+++ kirokushi/Readme.txt
@@ -0,0 +1,10 @@
+					Apr/20/2025
+
+./go_get.sh
+
+./go_list_gen.sh
+
+http-server
+
+./go_sync.sh
+
--- kirokushi/go_synch.sh
+++ kirokushi/go_synch.sh
@@ -1,4 +1,8 @@
 #
+#	browser/go_sync.sh
+#
+#					Jun/02/2025
+#
 SRC="./"
 TARGET="/home/uchida/html/kirokushi"
 rsync -avzz --delete $SRC"/" $TARGET
